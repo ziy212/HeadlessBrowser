b715620bc1fe7b72c618243b1ebc83350a8e15b2
xph906 <xiangpan2011@u.northwestern.edu>
2015-07-16 10:51:35 -0500

    dsd

---
+++ test.js
@@ -0,0 +1,27 @@
+ var page = require('webpage').create();
+ var server = require('webserver').create();
+ var system = require('system'),
+ url = system.args[1];
+ page.onResourceReceived = function (res) {
+ 	//console.log(res.redirectURL);
+      response_count++;
+      if (res.redirectURL) {
+          console.log(res.redirectURL)
+      }
+    };
+
+ page.onLoadStarted = function() {
+  console.log('Current page ' + window.location);
+  console.log('Now loading a new page...');
+};
+
+ page.open(url,function (status) {
+
+    if (status !== 'success') {
+        console.log('Unable to post!');
+    } else {
+        console.log(page.content.length);
+        console.log(page.url);
+    }
+    phantom.exit();
+});
