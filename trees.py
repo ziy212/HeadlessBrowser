2c4a7e8a15af91d59b4aa678551cad81127d6d51
xph906 <xiangpan2011@u.northwestern.edu>
2015-07-21 01:27:33 -0500

    update

--- trees.py
+++ trees.py
@@ -205,7 +205,7 @@ def main():
   #matchTreesWithScriptsFromURLList(sys.argv[1], sys.argv[2])
   #matchTreesFromDomainWithScriptsFromURLList(sys.argv[1], sys.argv[2])
   #getTrees(sys.argv[1])
-  rs = matchTreesFromDomainWithScript(open(sys.argv[1]).read())
+  rs = matchTreesFromDomainWithScript(sys.argv[1], open(sys.argv[2]).read())
   for item in rs:
     print '[S] %s' % (item)
 
