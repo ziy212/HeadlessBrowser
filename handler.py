aa642a243866cebb57c03529a929a7bd26a2054e
Xiang Pan <xiangpan2011@gmail.com>
2015-07-22 11:36:29 -0500

    update

--- handler.py
+++ handler.py
@@ -14,7 +14,7 @@ from ASTAnalyzer import ASTOutputNode
 from utilities import displayErrorMsg
 from base64 import b64encode
 from base64 import b64decode
-import sys, os, re, json, hashlib
+import sys, os, re, json, hashlib, uuid
 from strings import NodePattern
 from strings import analyzeStringListType
 
@@ -669,7 +669,8 @@ def analyzeObjectResultHelper(script_list, index):
   return rs
 
 def arrayToDict(arr):
-  print >> sys.stderr,"[START] arrayToDict:",str(arr)
+  debug_id = str(uuid.uuid4())
+  print >> sys.stderr,"[START:%s] arrayToDict:%s" %(debug_id, (arr))
   rs = {}
   if arr == None:
     print "ERROR: arr is NULL in arrayToDict"
@@ -682,21 +683,23 @@ def arrayToDict(arr):
         else:
           rs['basestring_'].append(obj)
       elif isinstance(obj, list):
-        print >> sys.stderr, "DEBUG1 ",str(obj)
+        print >> sys.stderr, "DEBUG1 [%s] list %s" %(debug_id, str(obj))
         subarr = extractArrayValues(obj)
         subrs = arrayToDict(subarr)
         mergeTwoArrayDict(rs,subrs)
-        print >> sys.stderr, "DEBUG2 ",str(obj)
+        print >> sys.stderr, "DEBUG2 [%s] list %s" %(debug_id, str(obj))
       elif isinstance(obj, dict):
+        print >> sys.stderr, "DEBUG1 [%s] dict %s" %(debug_id, str(obj))
         for k in obj:
           if not k in rs:
             rs[k] = [obj[k]]
           else:
             rs[k].append(obj[k])
+        print >> sys.stderr, "DEBUG2 [%s] dict %s" %(debug_id, str(obj))
     
   except Exception as e:
     displayErrorMsg('arrayToDict',str(e)+' '+str(arr))
-  print >> sys.stderr, "[END] arrayToDict:",str(arr),' TO ',str(rs)
+  print >> sys.stderr,"[  END:%s] arrayToDict:%s" %(debug_id, (arr))
 
 def analyzeArrayResultHelper(script_list, index):
   script_len = len(script_list)
